300abae91d61fd76628f24aa8186b32b0c2d10d5
User <user@example.com>
2025-10-28 19:40:16 -0700

    update `main.rs` to include some basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::prelude::*;
+use std::io::stdout;
 
 fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+    
+    // clean up
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    
     Ok(())
 }
